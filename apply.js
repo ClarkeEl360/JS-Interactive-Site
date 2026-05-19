f472ede86092f6d82343f5ff18c5294f977e9430
ClarkeEl360 <ClarkeEl360@northwested.org>
2026-05-19 11:32:03 -0400

    starting js for the form

--- apply.js
+++ apply.js
@@ -4,3 +4,25 @@
 // JS Interactive Site
 
 
+const form = document.getElementById("form");
+
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+
+const name_error = document.getElementById("name_error");
+const email_error = document.getElementById("email_error");
+
+form.addEventListener("submit", (e)=> {
+
+    var email_check = /[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}/;
+
+    if(name.value === "" || name.value == null) {
+        e.preventDefault();
+        name_error.innerHTML = "Name is required";
+    }
+
+    if(!email.value.match(email_check)){
+        e.preventDefault();
+        email_error.innerHTML = "Email is required";
+    }
+});
