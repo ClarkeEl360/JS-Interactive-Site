a00fd165c6acdbafdf0c7386fa341bb519eb9f4e
ClarkeEl360 <ClarkeEl360@northwested.org>
2026-05-20 10:46:42 -0400

    form created

--- apply.js
+++ apply.js
@@ -4,25 +4,51 @@
 // JS Interactive Site
 
 
-const form = document.getElementById("form");
+const left_form = document.getElementById("left_form");
 
-const name = document.getElementById("name");
-const email = document.getElementById("email");
+const left_name = document.getElementById("left_name");
+const left_email = document.getElementById("left_email");
 
-const name_error = document.getElementById("name_error");
-const email_error = document.getElementById("email_error");
+const left_name_error = document.getElementById("left_name_error");
+const left_email_error = document.getElementById("left_email_error");
 
-form.addEventListener("submit", (e)=> {
+
+
+const right_form = document.getElementById("right_form");
+
+const right_name = document.getElementById("right_name");
+const right_email = document.getElementById("right_email");
+
+const right_name_error = document.getElementById("right_name_error");
+const right_email_error = document.getElementById("right_email_error");
+
+
+left_form.addEventListener("submit", (e)=> {
 
     var email_check = /[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}/;
 
-    if(name.value === "" || name.value == null) {
+    if(left_name.value === "" || left_name.value == null) {
         e.preventDefault();
-        name_error.innerHTML = "Name is required";
+        left_name_error.innerHTML = "Name is required";
     }
 
-    if(!email.value.match(email_check)){
+    if(!left_email.value.match(email_check)){
         e.preventDefault();
-        email_error.innerHTML = "Email is required";
+        left_email_error.innerHTML = "Email is required";
+    }
+});
+
+right_form.addEventListener("submit", (e)=> {
+
+    var email_check = /[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}/;
+
+    if(right_name.value === "" || right_name.value == null) {
+        e.preventDefault();
+        right_name_error.innerHTML = "Name is required";
+    }
+
+    if(!right_email.value.match(email_check)){
+        e.preventDefault();
+        right_email_error.innerHTML = "Email is required";
     }
 });
